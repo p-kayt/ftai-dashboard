0859e954388eb8917e8c24e248fe1f81685ea174
Kayt <trieuphamkt59@gmail.com>
2024-04-06 22:16:32 +0700

    complete product manage

--- src/api/firebaseConfig.js
+++ src/api/firebaseConfig.js
@@ -1,27 +1,50 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDpAYYvlIcn5lQj5XbzwBZI1tFUn_1Ts1o",
+//   authDomain: "products-image.firebaseapp.com",
+//   projectId: "products-image",
+//   storageBucket: "products-image.appspot.com",
+//   messagingSenderId: "1057397225099",
+//   appId: "1:1057397225099:web:21d336e899bae2e3e4c68e"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDpAYYvlIcn5lQj5XbzwBZI1tFUn_1Ts1o",
-  authDomain: "products-image.firebaseapp.com",
-  projectId: "products-image",
-  storageBucket: "products-image.appspot.com",
-  messagingSenderId: "1057397225099",
-  appId: "1:1057397225099:web:21d336e899bae2e3e4c68e"
+  apiKey: "AIzaSyAi0XyLbQmHzvxCVaddl1ltBIGzB5FyR9M",
+  authDomain: "fsvton-18ce5.firebaseapp.com",
+  databaseURL: "https://fsvton-18ce5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fsvton-18ce5",
+  storageBucket: "fsvton-18ce5.appspot.com",
+  messagingSenderId: "225303706605",
+  appId: "1:225303706605:web:47239f1ef4436dad61c5e4",
+  measurementId: "G-XET61NGKMH"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const imgDb = getStorage(app);
 
 export const imageWithUrl = async ({ url }) => {
   //   console.log(url);
   const storage = getStorage();
-  const imageRef = ref(storage); // reference to the root of your storage
+  const imageRef = ref(storage, "ProductImg/"); // reference to the root of your storage
 
   // List all files in the bucket
   const res = await listAll(imageRef);
