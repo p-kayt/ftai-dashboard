0f19652879735ac402acd0c360a9a85638bb4827
Kayt <trieuphamkt59@gmail.com>
2024-04-11 22:45:38 +0700

    done models

--- src/api/modelsApi.js
+++ src/api/modelsApi.js
@@ -4,3 +4,23 @@ export const getAllModels = async (props) => {
   const res = await instance.get("/api/models");
   return res.data;
 };
+
+export const addModel = async (props) => {
+  console.log(props);
+  const { data } = props;
+  const res = await instance.post("/api/models", data);
+  return res.data;
+};
+
+export const updateModel = async (props) => {
+  const { id, data } = props;
+  console.log("api", props);
+  const res = await instance.put("/api/models/" + id, data);
+  return res.data;
+};
+
+export const deleteModelById = async (props) => {
+  const { id } = props;
+  const res = await instance.delete("/api/models/" + id);
+  return res.data;
+};
